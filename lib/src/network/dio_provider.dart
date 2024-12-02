39ebf67817d1f8b1629e55c72981f71f91d92c94
metinilgar <metinilgarmutlu@gmail.com>
2024-12-02 15:48:44 +0300

    Add DioExceptionMessage and dio provider

---
+++ lib/src/network/dio_provider.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'dio_provider.g.dart';
+
+@riverpod
+Dio dio(Ref ref) {
+  return Dio(
+    BaseOptions(
+        baseUrl: 'http://10.0.2.2:3000/api',
+        connectTimeout: const Duration(seconds: 5),
+        receiveTimeout: const Duration(seconds: 5),
+        responseType: ResponseType.json),
+  );
+}
